79c381b88025e500ca6a2d7d0a5675a0ad0d122b
Ayşe Firdevs Çelik <50719300+aysefirdevs@users.noreply.github.com>
2021-05-10 15:30:56 +0300

    kullanıcıya özel öneri sistemi son hali

--- oneri/views.py
+++ oneri/views.py
@@ -12,34 +12,34 @@ from yardim.models import Ihtiyac
 
 
 def veriokuma(request):
-    current_user = request.user
-    id=current_user.id
+    current_user = request.user  #giriş yapan kullanıcı alınıyor.
+    id=current_user.id           #giris yapan kullanici id si alıyoruz.
     db_connection_str = 'mysql+pymysql://root:1234@127.0.0.1/django_db'
     db_connection = create_engine(db_connection_str)
 
     df = pd.read_sql('SELECT * FROM yardim_arsiv', con=db_connection)        #veritabanında arsiv tablosuna bağlanıyorum.
-    select = df.loc[df['user_id']==id]             #kullanıcıların idlerini alıyorum burda.
+    select = df.loc[df['user_id']==id]             #arsiv tablosundan sadece giris yapmis kullanicinin verilerini çekiyorum.
 
 
     #print(select)
 
     a=' '
     vir=","
-    for x in select.content:
+    for x in select.content:        #contentleri birleştirip virgül ile ayırıyorum.
         a = x + vir + a
 
     #print(a)
 
-    metin = a.split(",")
-    sozluk = dict.fromkeys(metin, 0)
+    metin = a.split(",")                    # virgül gorduğumuz her kelimeyi split ile ayırıyorum.
+    sozluk = dict.fromkeys(metin, 0)        #virgülle ayırdığım kelimeleri sözlük haline getiriyorum.
 
-    for z in metin:
+    for z in metin:                         #metinde geçen kelimelerin kaç defa geçtiğini sayan döngü
         sozluk[z] += 1
 
     pd.DataFrame([sozluk])
 
 
-    def computeTF(wordDict, bow):
+    def computeTF(wordDict, bow):           #term fruquency algoritması kullanıldı.
         tfDict = {}
         bowCount = len(bow)
         for word, count in wordDict.items():
@@ -51,29 +51,28 @@ def veriokuma(request):
     tfidf=tf_df.idxmax(axis=1)    #tfidf içinde en yüksek değeri olan kelime bulunuyor.
     print(tf_df)
 
-    print("en çok kullanılan kelime", tfidf)  #en çok bağış yaptığı kelimeyi basıyotum.
+    print("en çok kullanılan kelime", tfidf)  #en çok bağış yaptığı kelimeyi terminale basıyorum.
 
 
-    iht = pd.read_sql('SELECT * FROM yardim_ihtiyac', con=db_connection)
-    ihtiyaclistele=Ihtiyac.objects.all()
+    iht = pd.read_sql('SELECT * FROM yardim_ihtiyac', con=db_connection)    #anasayfadaki ihtiyaçlar listesini vt den çekiyorum.
+    ihtiyaclistele=Ihtiyac.objects.all()        #html için gerekti görünmesi adına.
 
-    abc=str(tfidf)
+    abc=str(tfidf)      #en çok bağış yapılan kelimenin tf değerini stringe çevirdim.
     ayrik=abc.split(" ")
     temp=ayrik[5]
     eleman=temp.split("\n")
     kelime=eleman[0]
     sayac=121
-    gecici=list()
-    for f in iht.content:
+    gecici=list()           #gecici adında bir liste oluşturdum.
+    for f in iht.content:   #ihtiyac tablosundaki content kolonunu for döngüsüne aldım .
         sayac = sayac + 1
-        ayirma=f.split(" ")
-        for k in ayirma:
-            if kelime in k:
-                gecici.append(sayac)
+        ayirma=f.split(" ")     #içine girdiğim contentte boşluk gördükçe kelimeleri ayırdım.
+        for k in ayirma:        #ayırdığım keliemeleri döngüye alarak tek tek bakıyorum.
+            if kelime in k:     #kelime kullanıcın en çok bağış yaptığı ile ihtiyaç tablosundaki keliemelrden birini bulursa döngüye gir.
+                gecici.append(sayac)        #sayacta tuttuğu idyi gönderiyor ve idyi gecici listesine atıyorum.
                 print(gecici)
 
     #gerekli=Ihtiyac.objects.filter(id=gecici)
-
 
     return render(request, 'oneri/veriokuma.html',{'ihtiyaclistele':ihtiyaclistele,'gecici':gecici})
 
